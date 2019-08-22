5d748562cba7215f6d654849d7f6d0183c70ae2b
vanhwz <van136682262@gmail.com>
2019-08-22 11:43:58 +0800

    --8-21 commit

--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
