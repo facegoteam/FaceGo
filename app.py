43a84350ced475e390a4f33795e6d95d93c6fb7b
vanhwz <van136682262@gmail.com>
2019-08-27 08:44:24 +0800

    --8-25--commit

--- app.py
+++ app.py
@@ -1,26 +1,75 @@
-from flask import Flask, render_template, request, session
+from functools import wraps
+from flask import render_template, request, session, url_for, redirect
 import requests
 import json
+from models import *
 
-app = Flask(__name__)
-app.secret_key = 'super secret key'
 
+'''
+函数名：login
+创建时间：2019-08-23
+作者：黄文政
+说明：登录主页
+修改日期：2019-08-23
+'''
 @app.route('/')
 def login():
     return render_template('login/login.html')
 
+
+def login_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        # 从session获取用户信息，如果有，则用户已登录，否则没有登录
+        face_token = session.get('face_token')
+        print("session face_token:", face_token)
+        staff_id = session.get('staff_id')
+        print("staff_id:", staff_id)
+        if (not face_token) and (not staff_id):
+            # WITHOUT_LOGIN是一个常量
+            print(url_for('login', _external=True))
+            return redirect(url_for('login', _external=True))
+        else:
+            return func(*args, **kwargs)
+
+    return inner
+
+
+'''
+函数名：login_customer
+创建时间：2019-08-23
+作者：黄文政
+说明：顾客登录页面
+修改日期：2019-08-23
+'''
 @app.route('/login_customer')
 def login_customer():
     return render_template('login/login_customer.html')
 
+
+'''
+函数名：login_staff
+创建时间：2019-08-23
+作者：黄文政
+说明：管理员/收银员登录页面
+修改日期：2019-08-24
+'''
 @app.route('/login_staff')
 def login_staff():
     return render_template('login/login_staff.html')
 
+
+'''
+函数名：face_upload
+创建时间：2019-08-24
+作者：黄文政
+说明：处理上传的人脸，进行登录
+修改日期：2019-08-27
+'''
 @app.route('/face_upload', methods=['GET', 'POST'])
 def face_upload():
-    print('face upload call')
-    print(request.json)
+    # print('face upload call')
+    # print(request.json)
     if request.method == 'POST':
         img = request.files.get('file_obj')
         # path = './1.jpg'
@@ -40,12 +89,24 @@ def face_upload():
         print('faceupload:' + res_str)
         data = response.json()
         if len(data['faces']) != 0:
-            session['face_token'] = data['faces'][0]['face_token']
-        print(res_str)
+            print('confidence:' + str(data['results'][0]['confidence']))
+            if data['results'][0]['confidence'] > 80:
+                session['face_token'] = data['faces'][0]['face_token']
+                if session['staff_id']:
+                    session.pop('staff_id')
+        print('search:'+res_str)
         return res_str
     else:
         return "无图片"
 
+
+'''
+函数名：register
+创建时间：2019-08-24
+作者：黄文政
+说明：获得人脸，注册
+修改日期：2019-08-27
+'''
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     print('faceRegister call')
@@ -70,39 +131,125 @@ def register():
         }
         response = requests.post(url=url_addface, data=data_addface)
         session['face_token'] = face_token
+        if session['staff_id']:
+            session.pop('staff_id')
+
         return face_token
     else:
         return '无图片'
 
+
+'''
+函数名：customer
+创建时间：2019-08-25
+作者：黄文政
+说明：顾客主界面
+修改日期：2019-08-25
+'''
 @app.route('/customer')
+@login_required
 def customer():
     return render_template('customer/customer.html')
 
-@app.route('/customer_goods')
-def customer_goods():
-    return render_template('customer/customer_goods.html')
 
+@app.route('/customer_logout')
+@login_required
+def customer_logout():
+    session['face_token'] = None
+    return redirect(url_for('login'))
+
+
+'''
+函数名：customer_recommend
+创建时间：2019-08-25
+作者：黄文政
+说明：顾客商品推荐界面
+修改日期：2019-08-25
+'''
 @app.route('/customer_recommend')
+@login_required
 def customer_recommend():
     return render_template('customer/customer_recommend.html')
 
+
+'''
+函数名：customer_records
+创建时间：2019-08-25
+作者：黄文政
+说明：顾客商品推荐界面
+修改日期：2019-08-25
+'''
 @app.route('/customer_records')
+@login_required
 def customer_records():
     return render_template('customer/customer_records.html')
 
-@app.route('/cashier')
-def cashier():
-    return "cashier main page"
+'''
+方法名称：login_staff_test
+作者：姜子玥
+创建时间：2019-08-25
+说明：检测员工输入的密码与id是否符合,符合跳转到员工首页面，不符合弹出提示
+修改时间：2019-8-26
+'''
+@app.route('/staff_login', methods=['POST'])
+def staff_login():
+    user_id = request.form['id']
+    user_pwd = request.form['pwd']
+    # print(user_id)
+    # print(user_pwd)
+    user = Staff.query.filter_by(staff_id=user_id).first()
+    # print(user)
+    pwd = user.pwd
+    if user is None:
+        print('not user')
+        return 'fail'
+    elif pwd != user_pwd:
+        print('password error')
+        return 'fail'
+    else:
+        print("last")
+        role = user.identity
+        session['staff_id'] = user.staff_id
+        # if session['face_token']:
+        #     session.pop('face_token')
+        return role
 
+
+'''
+函数名：cashier_add_goods
+创建时间：2019-08-25
+作者：黄文政
+说明：收银员增加商品页面
+修改日期：2019-08-25
+'''
 @app.route('/cashier_add_goods')
+@login_required
 def cashier_add_goods():
     return render_template('cashier/cashier_add_goods.html')
 
+
+'''
+函数名：cashier_goods_details
+创建时间：2019-08-25
+作者：黄文政
+说明：收银员修改商品详情页面
+修改日期：2019-08-25
+'''
 @app.route('/cashier_goods_details')
+@login_required
 def cashier_goods_details():
     return render_template('cashier/cashier_goods_details.html')
 
+
+'''
+函数名：cashier_goods_add
+创建时间：2019-08-25
+作者：黄文政
+说明：收银员增加商品页面
+修改日期：2019-08-25
+'''
 @app.route('/cashier_goods_details_add', methods=['GET', 'POST'])
+@login_required
 def cashier_goods_add():
     form = request.form
     print(form)
@@ -118,7 +265,16 @@ def cashier_goods_add():
     finally:
         return render_template('cashier/cashier_management.html')
 
+
+'''
+函数名：cashier_goods_change
+创建时间：2019-08-25
+作者：黄文政
+说明：收银员增加商品请求
+修改日期：2019-08-25
+'''
 @app.route('/cashier_goods_details_change', methods=['GET', 'POST'])
+@login_required
 def cashier_goods_change():
     form = request.form
     print(form)
@@ -134,11 +290,24 @@ def cashier_goods_change():
     finally:
         render_template('cashier/cashier_management.html')
 
-@app.route('/cashier_management')
+
+'''
+函数名：cashier_management
+创建时间：2019-08-25
+作者：黄文政
+说明：收银员商品管理界面
+修改日期：2019-08-25
+'''
+@app.route('/cashier')
+@login_required
 def cashier_management():
     return render_template('cashier/cashier_management.html')
 
+@app.route('/test/<test_id>')
+def test(test_id):
+    print(test_id)
+    print(type(test_id))
+    return test_id
 
 if __name__ == '__main__':
-
     app.run()
---
+++ models.py
@@ -0,0 +1,115 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+pymysql.install_as_MySQLdb()
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+#配置数据库地址
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:8008208820@localhost/facego'
+app.config['SQLALCHEMY_TRABACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# 模型Staff
+# 创建时间：2019-08-25
+# 作者：姜子玥
+# 简要说明：用以存储Staff登陆的相关信息
+
+class Staff(db.Model):
+    __tablename__ = 'staff'  #表名staff
+    #字段
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    staff_id = db.Column(db.String(50), unique=True)
+    pwd = db.Column(db.String(50))
+    identity = db.Column(db.String(50))
+
+# 模型Customer
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以存储顾客的face_token信息
+class Customer(db.Model):
+    __tablename__ = 'customer'#表名customer
+    #字段
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    face_token = db.Column(db.String(50), unique=True)
+
+
+# 模型Goods
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以存储商品信息
+class Goods(db.Model):
+    __tablename__ = 'goods'
+    #字段
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    goods_name = db.Column(db.String(50), unique=True)
+    goods_type = db.Column(db.String(50), unique=True)
+    goods_count = db.Column(db.Integer)
+    goods_price = db.Column(db.Float)
+    goods_imag = db.Column(db.String(50))
+    goods_info = db.Column(db.Text)
+
+
+# 模型shop
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以存储商店信息
+class Shop(db.Model):
+    #shop表
+    __tablename__ = 'shop'
+    #字段
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    shop_name = db.Column(db.String(50), unique=True)
+    shop_address = db.Column(db.String(50), unique=True)
+    shop_phone = db.Column(db.String(50), unique=True)
+
+
+
+
+# 模型Records
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以记录消费信息
+class Records(db.Model):
+    #records表
+    __tablename__ = 'records'
+    #字段
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+
+    record_goods_count = db.Column(db.Integer())
+    record_goods_time = db.Column(db.DateTime)
+    record_goods_price = db.Column(db.Float)
+    face_token = db.Column(db.String(50))
+    shop_id = db.Column(db.Integer)
+    record_goods_id = db.Column(db.Integer)
+
+
+
+# 模型Cart
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以记录购物车信息
+class Cart(db.Model):
+    #cart表名
+    __tablename__ ='cart'
+
+    #字段
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    goods_id = db.Column(db.Integer, db.ForeignKey('goods.id'))
+    goods_count = db.Column(db.Integer)
+    face_token = db.Column(db.String(50))
+
+# 模型Orders
+# 创建时间：2019-08-26
+# 作者：姜子玥
+# 简要说明：用以记录下单列表
+class Orders(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    goods_id = db.Column(db.Integer, db.ForeignKey('goods.id'))
+    shop_id = db.Column(db.String(50))
+    goods_count = db.Column(db.Integer)
+    record_time = db.Column(db.DateTime)
+    face_token = db.Column(db.String(50))
+
